7e93d1fc60232d4115d6367ac791e10c62edc87f
Austin Barrett <austinbrrtt@gmail.com>
2021-03-21 00:49:54 -0400

    Initial Commit

---
+++ BarrettFiftyCalChallenge/BarrettFiftyCalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarrettFiftyCalChallengeApp.swift
+//  BarrettFiftyCalChallenge
+//
+//  Created by Austin Barrett on 3/21/21.
+//
+
+import SwiftUI
+
+@main
+struct BarrettFiftyCalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
