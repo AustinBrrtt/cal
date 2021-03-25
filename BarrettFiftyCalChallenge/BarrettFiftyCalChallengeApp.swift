10c017ff6cc673a81cedf5a45f75c6f90af2d827
Austin Barrett <austinbrrtt@gmail.com>
2021-03-25 10:09:33 -0400

    Created simple app for tracking calories burned daily

--- BarrettFiftyCalChallenge/BarrettFiftyCalChallengeApp.swift
+++ BarrettFiftyCalChallenge/BarrettFiftyCalChallengeApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct BarrettFiftyCalChallengeApp: App {
+    @StateObject var model: AppModel = AppModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(model)
         }
     }
 }
